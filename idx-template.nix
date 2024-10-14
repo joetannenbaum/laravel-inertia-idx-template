0b338d46e6b04c3f87f9360d617871cd2d4d11f5
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2024-10-14 14:21:53 -0400

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -15,7 +15,7 @@
   		cp ${./dev.nix} "$out"/.idx/dev.nix
       mkdir -p "$out"/resources/js/Pages
       cp ${./resources/${clientPackage}/app.js} "$out"/resources/js/app.js
-      cp ${./resources/${clientPackage}/vite.config.js} "$out"/resources/js/vite.config.js
+      cp ${./resources/${clientPackage}/vite.config.js} "$out"/vite.config.js
       cp ${./resources/${clientPackage}/Welcome.${if clientPackage == "react" then "jsx" else if clientPackage == "vue3" then "vue" else "svelte"}} "$out"/resources/js/Pages/Welcome.${if clientPackage == "react" then "jsx" else if clientPackage == "vue3" then "vue" else "svelte"}
       cp ${./resources/app.blade.php} "$out"/resources/views/app.blade.php
       cp ${./resources/app.php} "$out"/bootstrap/app.php
